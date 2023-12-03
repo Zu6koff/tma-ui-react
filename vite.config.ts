84486065a4ae0085684da644930dbc42fcd1230e
Misha Zubkov <m.zubkov@juicedev.org>
2023-12-03 05:42:37 +0300

    chore: base project configuration

---
+++ vite.config.ts
@@ -0,0 +1,45 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts';
+import {libInjectCss} from 'vite-plugin-lib-inject-css';
+
+import {glob} from 'glob';
+
+import { extname, relative, resolve } from 'path'
+import {fileURLToPath} from 'url';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    dts(),
+    libInjectCss()
+  ],
+  build: {
+    emptyOutDir: true,
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      formats: ['es']
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      input: Object.fromEntries(
+      glob.sync('src/**/*.{ts,tsx}').map(file => [
+        relative(
+          'src',
+          file.slice(0, file.length - extname(file).length)
+        ),
+        fileURLToPath(new URL(file, import.meta.url))
+      ])
+    ),
+    output: {
+      globals: {
+        react: "React",
+        "react-dom": "ReactDOM",
+      },
+      assetFileNames: 'assets/[name][extname]',
+      entryFileNames: '[name].js',
+    },
+    },
+  },
+})
